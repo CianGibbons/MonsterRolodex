98489ffb3798cd7b63fc27154f5a510fea3be23b
CianGibbons <ciangibb@gmail.com>
2020-06-21 14:26:52 +0100

    Updates

---
+++ precache-manifest.aa7f91198596f42bc7db30b2930cfa21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ef566e5486f8f0f4b86b8c0944c21a2",
+    "url": "/MonsterRolodex/index.html"
+  },
+  {
+    "revision": "eb6930cc81906b0351dc",
+    "url": "/MonsterRolodex/static/css/main.cda2dca4.chunk.css"
+  },
+  {
+    "revision": "86223520376e25c5e192",
+    "url": "/MonsterRolodex/static/js/2.7e118664.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonsterRolodex/static/js/2.7e118664.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb6930cc81906b0351dc",
+    "url": "/MonsterRolodex/static/js/main.b8283387.chunk.js"
+  },
+  {
+    "revision": "3f84de185d8ea49d2a1f",
+    "url": "/MonsterRolodex/static/js/runtime-main.1b676ad8.js"
+  }
+]);
